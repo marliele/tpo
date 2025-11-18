5c5027d1f64a8b86b748fe60dad0f46a79343c1d
marliele <alisasov@mail.ru>
2025-11-18 22:27:41 +0700

    Update openbmc_auth_tests.py

--- lab4/openbmc_auth_tests.py
+++ lab4/openbmc_auth_tests.py
@@ -4,17 +4,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 #Настройка драйвера, подключение Хрома
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
 
-#service = Service(ChromeDriverManager().install())
-#driver = webdriver.Chrome(service=service, options=options)
 driver_path = '/usr/bin/chromedriver'
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service, options=options) 
