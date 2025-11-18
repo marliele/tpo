50205ef70e988cd918c683463f3cfeba4eef53ee
marliele <alisasov@mail.ru>
2025-11-18 22:13:29 +0700

    Update openbmc_auth_tests.py

--- lab4/openbmc_auth_tests.py
+++ lab4/openbmc_auth_tests.py
@@ -12,7 +12,6 @@ options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-options.add_argument('--ignore-certificate-errors')
 
 #service = Service(ChromeDriverManager().install())
 #driver = webdriver.Chrome(service=service, options=options)
